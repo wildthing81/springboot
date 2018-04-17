4df6121fcc79831f3e834ab7b22efc49dbcaf0e7
wildthing81 <rama354@gmail.com>
2018-04-17 16:41:46 +1000

    payment rest controller

--- payment/src/main/java/com/ram/microservice/payment/PaymentApplication.java
+++ payment/src/main/java/com/ram/microservice/payment/PaymentApplication.java
@@ -2,8 +2,10 @@ package com.ram.microservice.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PaymentApplication {
 
 	public static void main(String[] args) {
---
+++ payment/src/main/java/com/ram/microservice/payment/controller/PaymentController.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.ram.microservice.payment.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wildthing
+ *
+ */
+@RestController
+public class PaymentController {
+
+}
