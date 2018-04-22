5f0b6eba78708b685fe704920d08311ebf77ed65
wildthing81 <rama354@gmail.com>
2018-04-22 15:55:21 +1000

    Payment Rest Repository

--- payment/src/main/java/com/ram/microservice/payment/repository/PaymentRepository.java
+++ payment/src/main/java/com/ram/microservice/payment/repository/PaymentRepository.java
@@ -1,7 +1,7 @@
 package com.ram.microservice.payment.repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
@@ -10,15 +10,11 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import com.ram.microservice.payment.domain.Payment;
 
 @RepositoryRestResource(collectionResourceRel="payment",path="payment")
-public interface PaymentRepository extends MongoRepository<Payment, String> {
+public interface PaymentRepository extends MongoRepository<Payment, String>{
 	
 	List<Payment> findByUserId(@Param("userId") String userId);
 	
-	@Override
-	default Optional<Payment> findById(String paymentId) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+	List<Payment> findByPymntDateTimeGreaterThan(@Param("pymntDateTime") LocalDateTime pymntDateTime);
 	
+	List<Payment> findByPymntDateTimeLessThan(@Param("pymntDateTime") LocalDateTime pymntDateTime);
 }
---
+++ payment/src/main/java/com/ram/microservice/payment/repository/PaymentRepositoryCustom.java
@@ -0,0 +1,12 @@
+package com.ram.microservice.payment.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.query.Param;
+
+import com.ram.microservice.payment.domain.Payment;
+
+public interface PaymentRepositoryCustom {
+
+	
+}
