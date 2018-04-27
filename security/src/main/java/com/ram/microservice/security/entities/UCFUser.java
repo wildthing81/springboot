172af1e8e6977eeadc2ae842623c52abd262e4f3
wildthing81 <rama354@gmail.com>
2018-04-27 17:33:14 +1000

    user dao & entities

---
+++ security/src/main/java/com/ram/microservice/security/entities/UCFUser.java
@@ -0,0 +1,116 @@
+package com.ram.microservice.security.entities;
+
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * @author asus
+ * 
+ */
+@Document(collection="user")
+public class UCFUser {
+
+	@Id
+	private String userId;
+
+	private String username;
+
+	private String firstname;
+
+	private String lastname;
+
+	private boolean isadmin;
+
+	private Boolean islocked;
+	
+	private String password;
+
+	private String emailid;
+
+	private Integer role;
+
+	public UCFUser(String userName, String firstName, String lastName,
+			String password, boolean isAdmin, Integer role) {
+		this.username = userName;
+		this.firstname = firstName;
+		this.lastname = lastName;
+		this.isadmin = isAdmin;
+		this.password = password;
+		this.role = role;
+	}
+
+	public UCFUser() {
+
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public boolean isIsadmin() {
+		return isadmin;
+	}
+
+	public void setIsadmin(boolean isadmin) {
+		this.isadmin = isadmin;
+	}
+
+	public Boolean getIsLocked() {
+		return islocked;
+	}
+
+	public void setIsLocked(Boolean isLocked) {
+		this.islocked = isLocked;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmailid() {
+		return emailid;
+	}
+
+	public void setEmailid(String emailid) {
+		this.emailid = emailid;
+	}
+
+	public Integer getRole() {
+		return role;
+	}
+
+	public void setRole(Integer role) {
+		this.role = role;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+//	 
+}
---
+++ security/src/main/java/com/ram/microservice/security/entities/UCFUserActivity.java
@@ -0,0 +1,60 @@
+package com.ram.microservice.security.entities;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+@Document(collection="useractivity")
+public class UCFUserActivity {
+
+    @Id
+    private String userActivityID;
+
+    private String userName;
+
+    private String activityKey;
+    
+    private String activityText;
+
+    private LocalDateTime activityTime;
+
+	public String getUserActivityID() {
+		return userActivityID;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getActivityKey() {
+		return activityKey;
+	}
+
+	public void setActivityKey(String activityKey) {
+		this.activityKey = activityKey;
+	}
+
+	public String getActivityText() {
+		return activityText;
+	}
+
+	public void setActivityText(String activityText) {
+		this.activityText = activityText;
+	}
+
+	public LocalDateTime getActivityTime() {
+		return activityTime;
+	}
+
+	public void setActivityTime(LocalDateTime loginTime) {
+		this.activityTime = loginTime;
+	}
+
+    
+}
