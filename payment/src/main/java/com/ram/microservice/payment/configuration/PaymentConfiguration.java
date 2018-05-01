c099b1e48d79fb3e7999b888bac46af78a259924
wildthing81 <rama354@gmail.com>
2018-05-01 15:40:00 +1000

    @ConfigurationProperties for payment values from application.properties

---
+++ payment/src/main/java/com/ram/microservice/payment/configuration/PaymentConfiguration.java
@@ -0,0 +1,11 @@
+package com.ram.microservice.payment.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component
+@Configuration("payment-service")
+public class PaymentConfiguration {
+
+	
+}
--- payment/src/main/java/com/ram/microservice/payment/service/impl/PaymentServiceImpl.java
+++ payment/src/main/java/com/ram/microservice/payment/service/impl/PaymentServiceImpl.java
@@ -1,7 +1,22 @@
 package com.ram.microservice.payment.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ram.microservice.payment.configuration.PaymentConfiguration;
+import com.ram.microservice.payment.domain.Payment;
 import com.ram.microservice.payment.service.PaymentService;
 
+@Service
 public class PaymentServiceImpl implements PaymentService {
+
+	@Autowired
+	PaymentConfiguration pymtConfiguration;
+	
+	@Override
+	public String makePayment(Payment payment) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
