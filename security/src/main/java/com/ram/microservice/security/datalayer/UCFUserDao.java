68714000a3437c94537d4b667282ac4fc675ecf0
wildthing81 <rama354@gmail.com>
2018-05-04 17:52:08 +1000

    rename dao to datalayer

--- security/src/main/java/com/ram/microservice/security/datalayer/UCFUserDao.java
+++ security/src/main/java/com/ram/microservice/security/datalayer/UCFUserDao.java
@@ -1,4 +1,4 @@
-package com.ram.microservice.security.dao;
+package com.ram.microservice.security.datalayer;
 
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
--- security/src/main/java/com/ram/microservice/security/datalayer/UserActivityDao.java
+++ security/src/main/java/com/ram/microservice/security/datalayer/UserActivityDao.java
@@ -1,4 +1,4 @@
-package com.ram.microservice.security.dao;
+package com.ram.microservice.security.datalayer;
 
 import java.time.LocalDateTime;
 import java.util.List;
