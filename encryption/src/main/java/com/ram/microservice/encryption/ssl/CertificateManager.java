be395d91f5b3ddb841d473d3fa2427ef18cd03ae
wildthing81 <rama354@gmail.com>
2018-05-04 17:37:38 +1000

    X509 certificate service for Secure HTTP

---
+++ encryption/src/main/java/com/ram/microservice/encryption/ssl/CertificateManager.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.ram.microservice.encryption.ssl;
+
+import java.security.cert.Certificate;
+
+import org.omg.CORBA.portable.InputStream;
+
+/**
+ * @author Admin
+ *
+ */
+public interface CertificateManager{
+
+	/**
+	 * 
+	 */
+		
+	public void exportCertificate(Certificate cert);
+
+
+	public Certificate importCertificate(InputStream is);
+	
+	
+	public String getCertificateType();
+	
+	
+	public Certificate generateCertificate(String host,int days,String certname,String sigalg);
+	
+}
